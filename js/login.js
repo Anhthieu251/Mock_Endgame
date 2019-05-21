44e0a56e51fe5e35d6d1c4b3fc76e00bb094e6af
thinhnguyen07 <theend_onthelove@yahoo.com.vn>
2019-05-21 08:37:53 +0700

    update login function

--- js/login.js
+++ js/login.js
@@ -10,3 +10,30 @@ validationApp.controller('mainController',function($scope){
     }
   }
 });
+
+
+
+function login(){
+
+  var userEmail = document.getElementById("email_field").value;
+  var userPass = document.getElementById("password_field").value;
+
+  firebase.auth().signInWithEmailAndPassword(userEmail, userPass).then(cred => {
+    console.log(cred.user);
+    location.assign("/interface/home.html");
+  });
+  firebase.auth().signInWithEmailAndPassword(userEmail, userPass)
+  .catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    window.alert("Error : " + errorMessage);
+
+    // ...
+  });
+
+}
+
+// function logout(){
+//   firebase.auth().signOut();
+// }
